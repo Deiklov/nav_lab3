1c1dc9df343af97c5da4fc61b10db0af296a4a15
Романов Андрей Владимирович <AVladiRomanov@sberbank.ru>
2022-04-20 08:39:35 +0300

    added angles

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas
 import pandas as pd
+from math import atan, degrees
 
 
 class GPS:
@@ -18,7 +18,7 @@ class GPS:
         self.x = self.data['x'][self.linestart:self.lineend].to_numpy(dtype=float)
         self.y = self.data['y'][self.linestart:self.lineend].to_numpy(dtype=float)
 
-    def read_data(self, name: str) -> pandas.DataFrame:
+    def read_data(self, name: str) -> pd.DataFrame:
         data = pd.read_csv(name, sep=",", header=None)
         data.columns = ["time", "x", "y", "z"]
         return data
@@ -45,6 +45,11 @@ class GPS:
         plt.legend()
         plt.show()
 
+    def calculate_angles(self, k1: float, k2: float) -> float:
+        angle_in_radians = atan(k1) - atan(k2)
+        angle_in_degrees = degrees(angle_in_radians)
+        return angle_in_degrees
+
     def task1(self):
         # y=kx+b
         kk, bb = self.LSM(self.x, self.y)
@@ -53,6 +58,11 @@ class GPS:
         plt.plot(self.x, self.y, 'o', label='Original data', markersize=3)
         plt.plot(self.x, kk * self.x + bb, 'r', label='LSM line', linewidth=3)
         plt.plot(self.x, kk2 * self.x + bb2, 'g', label='GLSM line')
+
+        plt.xlabel('X-coord (m)')
+        plt.ylabel('Y-coord (m)')
+        plt.title('Angles between line is {0} degrees'.format(self.calculate_angles(kk, kk2)))
+
         plt.legend()
         plt.show()
 
@@ -71,8 +81,12 @@ class GPS:
         plt.plot(time_step_arr, velocity_arr, 'y', label='Velocity original', linewidth=2)
         plt.plot(time_step_arr, kk * time_step_arr + bb, 'r', label='LSM line', linewidth=3)
         plt.plot(time_step_arr, kk2 * time_step_arr + bb2, 'g', label='GLSM line')
+        plt.xlabel('Time (sec)')
+        plt.ylabel('Velocity abs (m/s)')
+        plt.title('Angles between line is {0} degrees'.format(self.calculate_angles(kk, kk2)))
         plt.legend()
         plt.show()
+
 
 if __name__ == '__main__':
     gps = GPS('Rover1.cleanest')
